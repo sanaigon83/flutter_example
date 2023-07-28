c2969a1b8a62f7423341d2cd817da2c2238bdf8b
sanaigon83 <jk_sniper@naver.com>
2023-07-28 23:30:42 +0900

    part2 추가

---
+++ part2_instagram/lib/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'feed.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> images = [
+      "https://i.ibb.co/CQxfdHY/cat1.jpg",
+      "https://i.ibb.co/w6wxdrQ/cat2.jpg",
+      "https://i.ibb.co/GnwVqCd/cat3.jpg",
+      "https://i.ibb.co/1GMKYBy/cat4.jpg",
+      "https://i.ibb.co/cTGzTTX/cat5.jpg",
+      "https://i.ibb.co/47Y5Ct5/cat6.jpg",
+      "https://i.ibb.co/ZW38ngD/cat7.gif",
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.camera, color: Colors.black),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(CupertinoIcons.paperplane, color: Colors.black),
+            onPressed: () {},
+          )
+        ],
+        title: Image.asset('assets/logo.png', height: 32),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          final image = images[index];
+          return Feed(
+            imageUrl: image,
+          );
+        },
+      ),
+    );
+  }
+}
